3497223c3a8c191084701b930a57c565eca75144
Pietro Palombini <pietropalo@icloud.com>
2022-07-22 03:32:20 -0300

    Create ColoredP component

---
+++ components/ColoredP.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+const ColoredP = styled.p`
+  font-family: "Poppins", sans-serif;
+  font-size: 1.75rem;
+  font-weight: 300;
+  color: var(--accent-dark);
+`
+
+export default ColoredP
