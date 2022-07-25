4a750d2f874c8a4cc2f1e59cc141fdbc2d90de8b
Pietro Palombini <pietropalo@icloud.com>
2022-07-24 21:33:53 -0300

    Adapt Company

--- components/Home/Company.tsx
+++ components/Home/Company.tsx
@@ -1,18 +1,9 @@
 import Image from "next/image"
 import { useContext } from "react"
-import styled from "styled-components"
 
-import { ArrowIcon, Pad, StyledLink } from "components"
+import { ArrowIcon, ColoredP, Pad, StyledLink } from "components"
 import { SidePadding } from "lib/context/Home"
 import boxLogo from "public/Home/Company/box-logo.svg"
-
-const Paragraph = styled.p`
-  font-size: 1.75rem;
-  font-weight: 300;
-
-  text-align: center;
-  width: min(100%, 850px);
-`
 
 const Company = () => {
   const sidePadding = useContext(SidePadding)
@@ -35,12 +26,17 @@ const Company = () => {
             src={boxLogo}
           />
         </div>
-        <Paragraph className="mb-5">
+        <ColoredP
+          className="mb-5 text-center"
+          css={`
+            width: min(100%, 850px);
+          `}
+        >
           ¡Somos una compañía argentina apasionada por la innovación! Con más de
           25 años de experiencia y trayectoria, nuestra unidad de negocio
           “Software as a Service” revoluciona y simplifica la gestión de tus
           procesos de negocio a través de la digitalización.
-        </Paragraph>
+        </ColoredP>
         <StyledLink className="mb-5" href="https://boxcustodia.com">
           Conocé nuestra empresa Box <ArrowIcon word="custodia" />
         </StyledLink>
