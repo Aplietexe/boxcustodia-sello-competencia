0947a970aa11e89aaa45a55c5112c71ff9365005
Pietro Palombini <pietropalo@icloud.com>
2023-01-14 06:22:18 -0300

    Add missing fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,7 +27,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="es">
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
